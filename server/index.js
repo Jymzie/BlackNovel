8d64ed2a3868515657740c1c5f92eba9bd81f4bd
Jimwell Punzalan <punzalanjimwell@gmail.com>
2024-11-30 16:06:04 +0800

    testcors

--- server/index.js
+++ server/index.js
@@ -3,7 +3,12 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const cors = require('cors');
 app.use(cors());
-
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+  });
 
 app.get('/', (req, res) => {
     res.status(200).json('Data fetching...');
