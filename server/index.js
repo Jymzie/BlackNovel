0e11535bbf50c9345d5034e8597aebd42184d716
Jimwell Punzalan <punzalanjimwell@gmail.com>
2024-11-30 14:03:17 +0800

    proxy

--- server/index.js
+++ server/index.js
@@ -2,6 +2,11 @@ const express = require("express");
 
 const PORT = process.env.PORT || 3001;
 
+
+app.get('/', (req, res) => {
+    res.status(200).json('Data fetching...');
+  });
+
 const app = express();
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
