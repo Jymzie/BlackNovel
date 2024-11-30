4da48b92f220d3acaa48ca048b49af1952eb83ed
Jimwell Punzalan <punzalanjimwell@gmail.com>
2024-11-30 15:59:13 +0800

    cors

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const cors = require('cors');
+app.use(cors());
 
 
 app.get('/', (req, res) => {
